6221b519520832f9849258cb69d96e05c42ab386
jamessonjps <jamesson.jps@gmail.com>
2026-05-25 15:29:07 -0300

    fix: resolve syntax error in UserManagement map loop

--- src/pages/UserManagement.jsx
+++ src/pages/UserManagement.jsx
@@ -207,7 +207,8 @@ export default function UserManagement() {
                 </div>
               </div>
             </Card>
-          ))
+            );
+          })
         )}
       </div>
 
